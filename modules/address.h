fed67a687f601aeebc94e9ca0bacb21eee5702c7
leejoonli <git@github.com>
2023-02-01 21:35:39 -0800

    add address class

---
+++ modules/address.h
@@ -0,0 +1,48 @@
+#pragma once
+#ifndef ADDRESS
+#define ADDRESS
+
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <format>
+#include "urls.h"
+
+class Address {
+public:
+	std::string street;
+	std::string city;
+	std::string region;
+	std::string zip;
+	// change urls to class url. include urls header file after making it
+	std::string urls;
+	// country will be a variable declared in another header file
+	std::string country;
+
+	void make_address(std::string a_street, std::string a_city, std::string a_region, std::string a_zip, std::string COUNTRY_USA) {
+		street = a_street;
+		city = a_city;
+		region = a_region;
+		zip = a_zip;
+		/*
+		urls = Urls();
+		*/
+		country = COUNTRY_USA;
+	}
+
+	// NOT VOID RETURN TYPE.  CHANGE LATER
+	Address data() {
+		return *this;
+	}
+
+	/*char* get_address_line_1() {
+		return this->street;
+	}
+
+	char* get_address_line_2() {
+		char* temp[] = this->city + ", " + this.region + ", " + this.zip;
+		return temp;
+	}*/
+};
+
+#endif
---
+++ modules/urls.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef URLS
+#define URLS
+
+#include <string>
+
+std::string COUNTRY_USA = "us";
+
+#endif
